7e7aac6d23771ab7e07544423cbdbda233d9796e
mohamed-abdalla-dev <mohamadabdalla6818@gmail.com>
2026-08-09 20:21:39 +0200

    Upload Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router'
-import HomePage from './pages/HomePage'
-import AllProjects from './pages/AllProjects'
-import './index.css'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router";
+import HomePage from "./pages/HomePage";
+import AllProjects from "./pages/AllProjects";
+import ScrollToTop from "./components/ScrollToTop";
+import "./index.css";
+import "./App.css";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 text-white">
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -14,7 +21,7 @@ function App() {
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
