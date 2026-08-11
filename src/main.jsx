5091a67ce9469d87f5a11da7e2233d76f3950ad2
mohamed-abdalla-dev <mohamadabdalla6818@gmail.com>
2026-08-11 14:50:20 +0200

    Upload Project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const loader = document.getElementById("app-loader");
+const rootElement = document.getElementById("root");
+
+const hideLoader = () => {
+  if (!loader) return;
+  loader.classList.add("is-hidden");
+  window.setTimeout(() => loader.remove(), 350);
+};
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+hideLoader();
